36df7c7ec72ee65df01ba13039c28a91a0e8c783
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2025-02-14 11:31:41 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -9,6 +9,8 @@ export const connectDB = async () => {
     const res = await mongoose.connect(process.env.mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,  
+      socketTimeoutMS: 45000, 
     });
     console.log(`mongodb connected with server ${res.connection.host}`);
   } catch (error) {
